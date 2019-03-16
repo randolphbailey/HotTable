82c702de5a117c573c6fd38f3fae16800d2608b1
Dina Khouri <dina.khouri.91@gmail.com>
2019-03-16 11:02:51 -0500

    comment

--- server.js
+++ server.js
@@ -1,38 +1,33 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//ROUTING
 
-//ROUTING 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname, "index.html"));
-})
+app.get("/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
 
-app.get('/add', (req,res)=>{
-    res.sendFile(path.join(__dirname, "add.html"));
-})
-
-app.get('/view', (req,res)=>{
-    res.sendFile(path.join(__dirname, "view.html"));
-})
+app.get("/view", (req, res) => {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
 
 app.get("/api/tables", function(req, res) {
   //return stuff in tables.js
 });
 
 app.get("/api/waitlist", function(req, res) {
-    //return stuff in waitlist.js
+  //return stuff in waitlist.js
 });
 
 //LISTENING
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-Collapse
-
-
-
+  console.log("App listening on PORT " + PORT);
+});
