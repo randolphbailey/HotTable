6011a4926d14d76a6f6aa1d586d5867bf69f11ef
Lucas Merat <lucas.merat@gmail.com>
2019-03-16 12:16:08 -0500

    Added request thing

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const fs = require('fs');
+var bodyParser = require('body-parser');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var reservations;
 
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, "../", "home.html"));
@@ -16,12 +22,31 @@ app.get('/reserve', (req,res)=>{
 })
 
 app.get("/api/tables", function(req, res) {
-    var something;
+    let something;
   fs.readFile(path.join(__dirname, "../", "tables.js"), "utf-8", function(err, data) {
         something = JSON.parse(data);
         console.log(something);
         res.json(something);
   });
+});
+
+app.post("/api/tables", function(req, res) {
+    fs.readFile(path.join(__dirname, "../", "tables.js"), "utf-8", function(err, data) {
+        reservations = JSON.parse(data);
+        console.log("The length is " + reservations.length);
+        writeFile(reservations, res);
+    });
+    function writeFile(reservations, res){
+        // if(reservations.length < 5){
+        //     fs.
+        // }
+        console.log(reservations);
+    }
+//   fs.writeFile(path.join(__dirname, "../", "tables.js"), "utf-8", function(err, data) {
+//         something = JSON.parse(data);
+//         console.log(something);
+//         res.json(something);
+//   });
 });
 
 app.post("/api/waitlist", function(req, res) {
